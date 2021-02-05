64cde3991b5a0be2c30a2e0b445107701a3cf320
abduljeleelng <abduljeleelng@gmail.com>
2021-02-05 20:58:49 +0100

    pretify docu

--- routes/admin.js
+++ routes/admin.js
@@ -7,16 +7,17 @@ const {create, createDistric, districtList, districtById, oneDistrict, updateDis
 const {adminSignUpValidator} = require("../helpers/validator")
 
 
-
-router.post("/admin/signin", signin);
-router.post("/admin/forgetPassword", forgetPassword);
-router.post("/admin/resetPassword", resetPassword);
 router.post("/admin", requiredSignin, isSuperAdmin, create);
 router.post("/admin/district",requiredSignin, isSuperAdmin, createDistric);
 router.get("/admin/district", districtList);
 router.get("/admin/district/:districtId", oneDistrict);
 router.put("/admin/district/:districtId", updateDistrict);
 router.delete("/admin/district/:districtId", deleteDistrict);
+
+router.post("/admin/signin", signin);
+router.post("/admin/forgetPassword", forgetPassword);
+router.post("/admin/resetPassword", resetPassword);
+
 
 router.param("districtId",districtById)
 
