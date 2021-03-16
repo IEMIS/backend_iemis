90af15fe58ddeeed2c8890dba861b7dcb4d6413e
abduljeleelng <abduljeleelng@gmail.com>
2021-03-16 18:07:03 +0100

    App structures

---
+++ src/dbConnection/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const port = process.env.PORT || 9000;
+
+exports.dbConnection = async ()=>{
+    mongoose
+    .connect(process.env.MONGO_URI, {useCreateIndex: true,useUnifiedTopology:true, useNewUrlParser:true, keepAlive:true,})
+    .then(() => console.log(`Database is connected on port ${port}`));
+    mongoose.connection.on("error", err => {
+        console.log(`DB connection error: ${err.message}`);
+    });
+}
