8dd86ceaa374db7d570f51493c747144f8b8d4db
Olarotimi84 <hamasoasis84@gmail.com>
2021-04-02 20:47:43 +1200

    school middleware validator

---
+++ src/app/schools/midleware/validator/index.js
@@ -0,0 +1,37 @@
+exports.schoolCreator = (req, res, next) => {
+    req.check('code', 'school code is required').notEmpty();
+    req.check('district', 'school district is required').notEmpty();
+    req.check('names', 'school Name is required').notEmpty();
+    req.check('email', 'valid email address required')
+        .matches(/.+\@.+\..+/)
+        .withMessage('valid email address required')
+        .isLength({
+            min: 4,
+            max: 32
+        });
+    req.check('contact.fax', 'valid school fax number is required').notEmpty();
+    req.check('contact.mailBox', 'Mailing Address is missing').notEmpty();
+    //minimum phone digit in fiji is 7
+     req.check('contact.phone','enter a phone number').notEmpty();
+     req.check('contact.phone')
+        .isLength({min:7}).withMessage('phone number must be valid')
+        .matches(/\d/).withMessage('phone number must be a plain number');
+    req.check('contact.province', 'province is required').notEmpty();
+    req.check('eduLevel', 'Education Level is required').notEmpty();
+    req.check('ownership', 'ownership is required').notEmpty();
+    req.check('estabYear', 'Establishment Year is missing').notEmpty();
+    req.check('schoolCat', 'School Category is required').notEmpty();
+    req.check('schoolType', 'School Type is required').notEmpty();
+    req.check('password', 'Password is required').notEmpty();
+    req.check('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must contain at least 6 characters')
+        .matches(/\d/)
+        .withMessage('Password must contain a number');
+    const errors = req.validationErrors();
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    next();
+};
