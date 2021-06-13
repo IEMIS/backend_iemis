164972b1d372d556abde8567d2e4cdc3a3a0b3a2
Olarotimi84 <hamasoasis84@gmail.com>
2021-06-13 17:07:44 +1200

    model fixed

--- src/models/staff.js
+++ src/models/staff.js
@@ -37,7 +37,7 @@ const staffSchema = mongoose.Schema({
     title:{
         type: String,
         trim: true,
-        required: true,
+        //required: true,
     },
     gender:{
         type: String,
