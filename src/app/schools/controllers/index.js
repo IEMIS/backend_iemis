4be395104f8508ca94ddd14f8778b2dae919ee68
abduljeleelng <abduljeleelng@gmail.com>
2021-10-05 09:05:23 +0100

    school services controller

--- src/app/schools/controllers/index.js
+++ src/app/schools/controllers/index.js
@@ -1,65 +1,43 @@
+
+
+
+
+/* 
 import _ from 'lodash';
-import consola from 'consola';
 import * as models from '../../../models'
 
-/**
- * create a new school
- * @param {*} req 
- * @param {*} res 
- */
+
 exports.create = async (req, res) =>{
     const  {email}= req.body 
 
     const isSchool = await models.School.findOne({email})
     if(isSchool){
-         /***
-          * 
-          */
+
         return res.status(400).json({"error":"School email already exist"})
     }
     const schoo = new models.School(req.body)
     schoo.save((err, scho)=>{
         //consola.success({err, scho})
         if(err || !scho){
-             /**
-              * 
-              */
+   
             return  res.status(405).json({"error":"error in creating a school"})
         }
         res.status(200).json({"message":"school is created", school:scho})
     })
 }
 
-/**
- * get all the school
- * @param {*} req 
- * @param {*} res 
- * @returns 
- */
 export const schools = async (req, res)=>{
    const data = await models.School.find()
    if(!data){
-       /**
-        * docs
-        */
        return res.status(404).json({error:"fails to get users"})
    }
    res.json({message:"schools successfully fetched", data})
 }
 
-/**
- * school by Id
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- * @param {*} id 
- */
 export const schoolById = async (req, res, next, id) =>{
     models.School.findById(id).exec((err, data)=>{
         if(err || !data){
-            /**
-             * 
-             */
+        
             res.status(404).json({"error":"school not found"})
         }
         req.school = data;
@@ -67,22 +45,10 @@ export const schoolById = async (req, res, next, id) =>{
     });
 }
 
-/**
- * get Single school via school by Id
- * @param {*} req 
- * @param {*} res 
- * @returns 
- */
 
 export const school = async(req, res) =>{
     return res.status(200).json({message:"school successfully fetched", data:req.school})
 }
-
-/**
- * update school via school by Id
- * @param {*} req 
- * @param {*} res 
-*/
 
 export const updateSchool = async (req, res)=>{
     let update = _.extend(req.school,req.body)
@@ -95,13 +61,6 @@ export const updateSchool = async (req, res)=>{
     })
 
 }
-
-/**
-  * delete School vai school by Id
-  * @param {*} req 
-  * @param {*} res 
-*/
-
 export const deleteSchool = async (req, res)=>{
      let schol = req.school;
      schol.remove((err, data)=>{
@@ -111,13 +70,6 @@ export const deleteSchool = async (req, res)=>{
          res.status(200).json({message:"school succesfully deleted", data})
      })
 }
-
-/**
- * count all the school
- * @param {*} req 
- * @param {*} res 
- */
-
 export const countSchool = async (req, res)=>{
     let count = await models.School.countDocuments();
     if(!count){
@@ -125,20 +77,10 @@ export const countSchool = async (req, res)=>{
     }
     res.status(200).json({message:"schools successfully counted", data:count})
 }
-
-
-/**
- * get district a school by long to via school by Id
- * @param {*} req 
- * @param {*} res 
- */
 export const schoolbelongtoDistrict = async (req, res)=>{
     let {_id} = req.school;
     models.School.find().populate('district').exec((err, data)=>{
         console.log({data, err})
-        /**
-         * docs
-         */
         if(err || !data){
             return res.status(403).json({error:"fail to get district", err})
         }
@@ -150,17 +92,12 @@ export const studentInSchool = async (req, res )=>{
     const schol = req.school;
     schol.populate('student').exec((err, student)=>{
         if(err || !student){
-            /**
-             * 
-             */
             return res.status(400).json({"error":"fail", err})
         }
         let total = student.countDocuments()
         res.status(200).json({"message":"success", data:{student, total}})
     })
 }
-
-
 
 export const schoolByDistrict = async (req, res, next, id)=>{
      //models.District.findById(id).exec((err, district)=>{
@@ -169,10 +106,6 @@ export const schoolByDistrict = async (req, res, next, id)=>{
         .sort({ district: -1, school:-1 })
         .exec((err, district)=>{
          if(err || !district){
-             /**
-              * 
-              * 
-              */
              res.status(404).json({"error":"District not exist"})
          }
          req.schoolByDistrict = district;
@@ -189,4 +122,6 @@ export const schoolInDistrcitCount = async(req, res) =>{
     //data.countDocuments();
     return res.status(200).json({message:"school successfully fetched", data:req.schoolByDistrict,})
 }
+
+*/
 
