1e97ec82d6d89842c1934bf840e30fa4053bf3c1
Abduljeleel Yusuff <abduljeleelng@gmail.com>
2021-11-03 18:15:07 +0100

    Update index.js
    Routes student by district , and student by school
    
    URL 
    -  /admin/students/by/district/:district
    - /admin/students/by/school/:school

--- src/app/admin/routes/index.js
+++ src/app/admin/routes/index.js
@@ -39,7 +39,10 @@ router.delete("/admin/school/:schoolId", Mid.requiredSignin, Mid.isSuperAdmin, C
 /**
  *  Student Services 
 */
+// studentByDistrict_d 
 router.get("/admin/students", Mid.requiredSignin, Mid.isSuperAdmin, Ctr.student);
+router.get("/admin/students/by/district/:district", Mid.requiredSignin, Mid.isSuperAdmin, Ctr.studentByDistrict_d);
+router.get("/admin/students/by/school/:school", Mid.requiredSignin, Mid.isSuperAdmin, Ctr.studentBySchool_d);
 router.post("/admin/developer/student", Ctr.createBulkStudent)
 router.post("/admin/student",Valid.studentCreator, Mid.requiredSignin, Mid.isSuperAdmin, Ctr.createStudent)
 router.get("/admin/students/:studentId",Mid.requiredSignin, Mid.isSuperAdmin, Ctr.student)
